0a1503e90f7568958a96a0f31ddfe1638d54f48b
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-04-23 21:32:23 +0000

    Added package info.

---
+++ package-info.java
@@ -0,0 +1,9 @@
+/** .
+* This is the package info for Factorial.java
+*
+* This program uses recursion to calculate the factorial of a number.
+*
+* @author  Zak Goneau
+* @version 1.0
+* @since   2025-04-23
+*/
